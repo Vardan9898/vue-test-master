fd1fee9d6ce0f0b2df220817506d658db935b7cc
Vardan <vardannikolayan1@gmail.com>
2022-06-22 18:09:20 +0400

    first commit

---
+++ resources/js/Pages/Tasks/Form.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container mx-auto mt-5">
+        <div class="">
+            <div class="flex justify-center mb-5">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 mr-2">
+                    <path
+                        d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM232 152C232 138.8 242.8 128 256 128s24 10.75 24 24v128c0 13.25-10.75 24-24 24S232 293.3 232 280V152zM256 400c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 385.9 273.4 400 256 400z"/>
+                </svg>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dui diam, auctor sed placerat id, egestas
+                    a purus. Aliquam in orci sit amet magna mattis varius</p>
+            </div>
+            <div class="bg-gray-300">
+                ejfhuehfu
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
