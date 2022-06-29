2e7b5e312b1da2588c893358a50894b39d7bcf3c
Vardan <vardannikolayan1@gmail.com>
2022-06-29 10:16:04 +0400

    test

--- resources/js/Pages/Tasks/Form.vue
+++ resources/js/Pages/Tasks/Form.vue
@@ -29,42 +29,92 @@
                     Liebe Claudia,
                     entschuldige, dass ich mich so lange nicht bei dir gemeldet
                     Wie du weißt, bin ich vor zwei Wochen
-                <div id="div1" @ondrop="drop($event)" @ondragover="allowDrop($event)"></div>
-                Deswegen hatte ich leider keine Zeit .... meine Freunde.
-                Aber deinen Vorschlag, mal wieder gemeinsam .... Tag miteinander zu verbringen, finde ich sehr gut.
-                Mir passt es auch am besten am Wochenende. Würde es bei dir schon .... Samstag gehen? Da habe ich
-                noch nichts vor. Es würde .... natürlich freuen, wenn du dir bei dieser Gelegenheit auch meine neue
-                Wohnung anschaust. Es ist wunderbar, so viel zu haben. Was meinst du, wir uns bei mir treffen und
-                dann unsere Einkaufstour machen? Oder ist es dir .... , wenn wir zuerst einkaufen und am Abend
-                zusammen kochen und essen? Übrigens, können wir dein Auto nehmen? .... steht schon wieder in der
-                Werkstatt. Schreib mir doch bald, ob du an diesem Tag Zeit hast. .... du keine Zeit hast, finden wir
-                sicher einen anderen Tag.
-                Marion
+                    Deswegen hatte ich leider keine Zeit
+                    <div class="droptarget area inline-block relative" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+<!--                        <button id="x-button" class="x-button absolute hidden"></button>-->
+                    </div>
+                    meine Freunde.
+                    Aber deinen Vorschlag, mal wieder gemeinsam
+                    <div class="droptarget area inline-block" @ondrop="drop($event)" @ondragover="allowDrop($event)"></div>
+                    Tag miteinander zu verbringen, finde ich sehr gut.
+                    Mir passt es auch am besten am Wochenende. Würde es bei dir schon
+                    <div class="droptarget area inline-block" @ondrop="drop($event)" @ondragover="allowDrop($event)"></div>
+                    Samstag gehen? Da habe ich
+                    noch nichts vor. Es würde
+                    <div class="droptarget area inline-block" @ondrop="drop($event)" @ondragover="allowDrop($event)"></div>
+                    natürlich freuen, wenn du dir bei dieser Gelegenheit auch meine neue
+                    Wohnung anschaust. Es ist wunderbar, so viel zu haben. Was meinst du, wir uns bei mir treffen und
+                    dann unsere Einkaufstour machen? Oder ist es dir
+                    <div class="droptarget area inline-block" @ondrop="drop($event)" @ondragover="allowDrop($event)"></div>
+                    , wenn wir zuerst einkaufen und am Abend
+                    zusammen kochen und essen? Übrigens, können wir dein Auto nehmen?
+                    <div class="droptarget area inline-block" @ondrop="drop($event)" @ondragover="allowDrop($event)"></div>
+                    steht schon wieder in der
+                    Werkstatt. Schreib mir doch bald, ob du an diesem Tag Zeit hast.
+                    <div class="droptarget area inline-block" @ondrop="drop($event)" @ondragover="allowDrop($event)"></div>
+                    du keine Zeit hast, finden wir
+                    sicher einen anderen Tag.
+                    Marion
                 </div>
             </div>
 
             <hr class="mb-6 border-dashed">
 
-            <p id="drag1" draggable="true" @ondragstart="drag($event)">This is a draggable paragraph. Drag this element into the rectangle.</p>
+            <div class="grid grid-cols-4 gap-4">
+                <div class="droptarget area justify-items-center" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+                    <p class="flex justify-center" @ondragstart="dragStart($event)" draggable="true" id="dragtarget">umgezogen</p>
+                </div>
+                <div class="droptarget1 area justify-items-center" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+                    <p class="flex justify-center" @ondragstart="dragStart($event)" draggable="true" id="dragtarget1">mich</p>
+                </div>
+                <div class="droptarget2 area justify-items-center" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+                    <p class="flex justify-center" @ondragstart="dragStart($event)" draggable="true" id="dragtarget2">einen</p>
+                </div>
+                <div class="droptarget3 area justify-items-center" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+                    <p class="flex justify-center" @ondragstart="dragStart($event)" draggable="true" id="dragtarget3">nächsten</p>
+                </div>
+                <div class="droptarget4 area justify-items-center" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+                    <p class="flex justify-center" @ondragstart="dragStart($event)" draggable="true" id="dragtarget4">für</p>
+                </div>
+                <div class="droptarget5 area justify-items-center" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+                    <p class="flex justify-center" @ondragstart="dragStart($event)" draggable="true" id="dragtarget5">lieber</p>
+                </div>
+                <div class="droptarget6 area justify-items-center" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+                    <p class="flex justify-center" @ondragstart="dragStart($event)" draggable="true" id="dragtarget6">wollen</p>
+                </div>
+                <div class="droptarget7 area justify-items-center" @ondrop="drop($event)" @ondragover="allowDrop($event)">
+                    <p class="flex justify-center" @ondragstart="dragStart($event)" draggable="true" id="dragtarget7">Meines</p>
+                </div>
+            </div>
+
+
+            <p id="demo"></p>
+
         </div>
     </div>
 </template>
 
 <script>
+document.addEventListener("dragstart", function (event) {
+    event.dataTransfer.setData("Text", event.target.id);
+    document.getElementById("demo").innerHTML = "Started to drag the p element.";
+});
+
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+});
+
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById("demo").innerHTML = "The p element was dropped";
+    document.getElementById('x-button').classList.remove('hidden');
+    document.getAttribute('draggable')
+});
+
 export default {
-    methods: {
-        drop(ev) {
-            let data = ev.dataTransfer.getData("Text");
-            ev.target.appendChild(document.getElementById(data));
-            ev.preventDefault();
-        },
-        allowDrop(ev) {
-            ev.preventDefault();
-        },
-        drag(ev) {
-            ev.dataTransfer.setData("Text", ev.target.id);
-        }
-    }
+    methods: {}
 };
 </script>
 <style scoped>
@@ -115,16 +165,30 @@ export default {
     background: #F3983F;
     font-size: 25px;
 }
+
 .line-h {
     line-height: 33px;
 }
+
 .main-text {
     line-height: 78px;
 }
-#div1 {
-    width: 350px;
-    height: 70px;
-    padding: 10px;
-    border: 1px solid #aaaaaa;
+
+.area {
+    height: 46px;
+    width: 189px;
+    border-radius: 10px;
+    background: #FFFFFF;
+    border: 1px solid rgba(0, 0, 0, 0.15);
+}
+
+.x-button {
+    height: 29px;
+    width: 29px;
+    left: 90%;
+    top: -36%;
+    background: #E83F6F;
+    border: 1px solid #E83F6F;
+    border-radius: 50%;
 }
 </style>
